df4b50f41a45724b78499b1a62415ae30175bccd
josep <jssdlcu1331@gmail.com>
2018-11-06 15:32:56 +0100

    initial commit

---
+++ Assets/Scripts/MovementRestricted.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRestricted : MonoBehaviour {
+	public GameObject ITarget;
+	public Transform ITargetT;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ITargetT= ITarget.GetComponent<Transform>();
+		transform.position = new Vector3(ITargetT.position.x*2,0.201f,ITargetT.position.z*2);
+	}
+}
